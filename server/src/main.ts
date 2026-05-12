1dd19ecc07dbbe7a047ae1b7855899f551d7a877
Nihashini <nihashinisuba@gmail.com>
2026-05-12 10:38:13 +0530

    some files updated

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin:"http://localhost:5173",
-    Credentials:true
+ app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
